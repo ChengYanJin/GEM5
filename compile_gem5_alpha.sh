716f34b254541da6bae472b686ebc4281650a04a
Orange <chengyanjin@163.com>
2016-08-02 15:20:02 +0800

    Add build and run scripts.

---
+++ compile_gem5_alpha.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+scons build/ALPHA_MESI_Two_Level/gem5.opt -j4
---
+++ run_gem5_alpha_parsec2.1.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+#
+# A Python script to run all multi-core experiments for PARSEC 2.1 benchmarks.
+#
+# Copyright (C) Min Cai 2015
+#
+
+import os
+import multiprocessing as mp
+
+
+def run(bench, input_set, l2_size, l2_assoc, num_threads):
+    dir = 'results/' + bench + '/' + input_set + '/' + l2_size + '/' + str(l2_assoc) + 'way/' + str(num_threads) + 'c/'
+
+    os.system('rm -fr ' + dir)
+    os.system('mkdir -p ' + dir)
+
+    cmd_run = 'build/ALPHA_MESI_Two_Level/gem5.opt -d ' + dir + ' configs/example/fs.py --cpu-type=timing --num-cpus=' \
+              + str(num_threads) + ' --script=ext/parsec/2.1/run_scripts/' \
+              + bench + '_' + str(num_threads) + 'c_' + input_set + '.rcS' \
+              + ' --caches --l2cache --num-l2caches=1' \
+              + ' --l1d_size=32kB --l1i_size=32kB --l2_size=' + l2_size + ' --l2_assoc=' + str(l2_assoc)
+    print(cmd_run)
+    os.system(cmd_run)
+
+
+def run_experiment(args):
+    bench, input_set, l2_size, l2_assoc, num_threads = args
+    run(bench, input_set, l2_size, l2_assoc, num_threads)
+
+experiments = []
+
+
+def run_experiments():
+    num_processes = mp.cpu_count()
+    pool = mp.Pool(num_processes)
+    pool.map(run_experiment, experiments)
+
+    pool.close()
+    pool.join()
+
+
+def add_experiment(bench, input_set, l2_size, l2_assoc, num_threads):
+    args = bench, input_set, l2_size, l2_assoc, num_threads
+    experiments.append(args)
+
+input_sets = ['simsmall']
+
+
+def add_experiments(bench, input_set):
+  #  add_experiment(bench, input_set, '256kB', 4, 4)
+    add_experiment(bench, input_set, '512kB', 32, 4)
+  
+
+for input_set in input_sets:
+    #add_experiments('blackscholes', input_set)
+    add_experiments('bodytrack', input_set)
+    add_experiments('canneal', input_set)
+   # add_experiments('dedup', input_set)
+   # add_tasks('facesim', input_set)
+    add_experiments('ferret', input_set)
+    add_experiments('fluidanimate', input_set)
+    # add_tasks('freqmine', input_set)
+    add_experiments('streamcluster', input_set)
+    add_experiments('swaptions', input_set)
+    # add_tasks('vips', input_set)
+    # add_tasks('x264', input_set)
+
+run_experiments()
